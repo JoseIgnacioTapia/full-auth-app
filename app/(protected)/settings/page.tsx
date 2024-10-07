75dadb76c42f2e040b122f8766687479ad5d4c1a
JoseIgnacioTapia <josignaciot@gmail.com>
2024-10-07 11:23:59 -0300

    Middleware & Login

---
+++ app/(protected)/settings/page.tsx
@@ -0,0 +1,9 @@
+import { auth } from '@/auth';
+
+async function SettingsPage() {
+  const session = await auth();
+
+  return <div>{JSON.stringify(session)}</div>;
+}
+
+export default SettingsPage;
